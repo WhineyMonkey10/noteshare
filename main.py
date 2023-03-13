a94a0c17d915d07eb3b7eff5b976968e3e8713f9
Gabriel <gbialystokjoly@gmail.com>
2023-03-13 12:21:44 +0100

    Login update

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from src.Database.database import Database
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 
 Database = Database()
 Database.insertNote("Jello is", "Yummy")
 
 app = Flask(__name__)
+app.secret_key = '12312381289389123891238912389213'
 
 @app.route('/')
 def index():
-    if request.session.get('logged_in') == True:
+    if 'logged_in' in session:
         notes = Database.getNotes()
         return render_template('index.html', notes=notes)
     else:
@@ -17,7 +18,7 @@ def index():
 # Page to load the note when user clicks on the title
 @app.route('/note/<id>')
 def note(id):
-    if request.session.get('logged_in') == True:
+    if 'logged_in' in session:
         noteTitle = Database.getNoteById(id);noteTitle = noteTitle['title']
         noteContent = Database.getNoteById(id);noteContent = noteContent['content']
         noteID = Database.getNoteById(id);noteID = noteID['_id']
@@ -27,7 +28,7 @@ def note(id):
 
 @app.route('/addNote', methods=['POST', 'GET'])
 def addNote():
-    if request.session.get('logged_in') == True:
+    if 'logged_in' in session:
         if request.method == 'POST':
             title = request.form['title']
             content = request.form['content']
@@ -39,18 +40,22 @@ def addNote():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    if request.method == 'POST':
-        username = request.form['Username']
-        password = request.form['Password']
-        if Database.login(username, password):
-            request.session['username'] = username
-            request.session['password'] = password
-            request.session['logged_in'] = True
-            return index()
-        else:
+    if 'logged_in' not in session:
+        if request.method == 'GET':
             return render_template('login.html')
-    return render_template('login.html')
+        if request.method == 'POST':
+            username = request.form['Username']
+            password = request.form['Password']
+            if Database.login(username, password):
+                session['username'] = username
+                session['password'] = password
+                session['logged_in'] = True
+                return index()
+            else:
+                return render_template('login.html')
+        return render_template('login.html')
+    else:
+        return index()
+
 
 app.run(host='0.0.0.0', port=5000)
